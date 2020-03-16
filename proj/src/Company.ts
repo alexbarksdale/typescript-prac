770a13f4bc6ce69018038ffc456e4391bf3f9bf1
alexbarksdale <ale.barksdale@gmail.com>
2020-03-16 01:59:34 -0500

    Project

---
+++ proj/src/Company.ts
@@ -0,0 +1,33 @@
+import faker from 'faker';
+import { Mappable } from './Map';
+
+export class Company implements Mappable {
+    name: string;
+    catchPhrase: string;
+    location: {
+        lat: number;
+        lng: number;
+    };
+
+    constructor() {
+        this.name = faker.company.companyName();
+        this.catchPhrase = faker.company.catchPhrase();
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        };
+    }
+
+    markerContent(): string {
+        return `
+        <div>
+            <h1>
+                Name: ${this.name}
+            </h1>
+            <h3>
+                Catch Phrase: ${this.catchPhrase}
+            </h3>
+        </div>
+        `;
+    }
+}
