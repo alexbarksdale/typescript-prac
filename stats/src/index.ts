b7e9fa2597efba3687aa7aac5befd09956bff618
alexbarksdale <ale.barksdale@gmail.com>
2020-04-04 23:39:54 -0500

    Enums in TS

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -9,4 +9,19 @@ const matches = fs
         return row.split(',');
     });
 
-console.log(matches);
+// The type would be 'MatchResult'
+enum MatchResult {
+    HomeWin = 'H',
+    AwayWin = 'A',
+    Draw = 'D',
+}
+
+let manUnitedWins = 0;
+for (let match of matches) {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWins += 1;
+    } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWins += 1;
+    }
+}
+console.log(manUnitedWins);
