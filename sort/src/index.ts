28d83adc9b503cb8e21a5c783206aa05c725e316
alexbarksdale <ale.barksdale@gmail.com>
2020-04-04 20:39:45 -0500

    [Add] Linked list

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -5,9 +5,10 @@ import { CharactersCollection } from './CharactersCollection';
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const numSorter = new Sorter(numbersCollection);
 numSorter.sort();
-console.log(numbersCollection.data);
+console.log('Number sort', numbersCollection.data);
 
 const charactersCollection = new CharactersCollection('Xaaa');
 const charSorter = new Sorter(charactersCollection);
 charSorter.sort();
-console.log(charactersCollection.data);
+console.log('Character Sort', charactersCollection.data);
+
