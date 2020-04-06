7f6d753b5f6085b9c28f065a41ad2b7e2d0114f4
alexbarksdale <ale.barksdale@gmail.com>
2020-04-06 00:09:04 -0500

    Refactor

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,11 +1,13 @@
 import { MatchReader } from './MatchReader';
 import { MatchResult } from './MathResult';
+import { CsvFileReader } from './CsvFileReader';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+const csvFileReader = new CsvFileReader('football.csv');
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load(); // matchReader.matches
 
 let manUnitedWins = 0;
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
     if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
         manUnitedWins += 1;
     } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
