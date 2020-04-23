e181d9170671c1f55f0f28181c9e990e5e80f9d9
alexbarksdale <ale.barksdale@gmail.com>
2020-04-23 02:11:52 -0500

    Redux with typescriopt

--- react-ts/src/components/App.tsx
+++ react-ts/src/components/App.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Todo, fetchTodos } from '../actions';
+import { StoreState } from '../reducers';
 
 interface AppProps {
-    color?: string;
+    todos: Todo[];
+    fetchTodos(): any;
 }
 
 // function App(props: AppProps): JSX.Element {
 //     return <h1>{props.color}</h1>;
 // }
 
-export default class App extends React.Component<AppProps> {
+class _App extends React.Component<AppProps> {
     render() {
         return <div>Hi</div>;
     }
 }
+
+const mapStateToProps = (state: StoreState): { todos: Todo[] } => {
+    return { todos: state.todos };
+};
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
